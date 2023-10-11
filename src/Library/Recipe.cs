c573550d0af072accb904ef044e3820f2d4399d9
JBevc <141636441+JBevc@users.noreply.github.com>
2023-10-11 10:33:00 -0300

    cambios en recipe

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -15,9 +15,11 @@ namespace Full_GRASP_And_SOLID
 
         public Product FinalProduct { get; set; }
 
-        public void AddStep(Step step)
+        public Step AddStep(Product input, double quantity, Equipment equipment, int time)
         {
-            this.steps.Add(step);
+            Step NewStep = new Step(input,quantity,equipment,time);
+            return NewStep;
+
         }
 
         public void RemoveStep(Step step)
